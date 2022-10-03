025bb61bcdadd279c069660ba284bd02afb63387
gitokonomi <114793944+gitokonomi@users.noreply.github.com>
2022-10-03 12:06:33 +0000

    [Add]編集、削除追加

--- config/routes.rb
+++ config/routes.rb
@@ -7,9 +7,6 @@ Rails.application.routes.draw do
 
   get 'books/:id/edit' => 'books#edit' , as: 'edit_book'
   patch 'books/:id' => 'books#update' , as: 'update_book'
-  # コピ
-
-
   delete 'books/:id' => 'books#destroy'
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
