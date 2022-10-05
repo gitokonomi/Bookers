9a55ae0a3dcf10a13479919bcf3b92a756a6f6af
gitokonomi <114793944+gitokonomi@users.noreply.github.com>
2022-10-05 07:35:10 +0000

    [Fix]エラー表示後の変遷、ルーティング見直し

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,7 @@
 Rails.application.routes.draw do
-  get 'books/new'
-  get 'books/index'
-  post 'books' => 'books#create'   #post
-  get 'books/:id' => 'books#show' , as: 'book'
   root :to => 'homes#top'
 
-  get 'books/:id/edit' => 'books#edit' , as: 'edit_book'
-  patch 'books/:id' => 'books#update' , as: 'update_book'
-  delete 'books/:id' => 'books#destroy' , as: 'delete_book'
+  resources :books
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
